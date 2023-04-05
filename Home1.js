1e681e07cdf8ce52eb73187e02e1101bea67696c
sasikumar-07 <128038363+sasikumar-07@users.noreply.github.com>
2023-04-05 14:34:37 +0530

    Create Home1.js

---
+++ Home1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home1 = () => {
+  return (
+    <div>
+      <h1>Welcome everyone!</h1>
+      <h2>You have reached the Best travel website !</h2>
+    </div>
+  )
+}
+
+export default Home1
